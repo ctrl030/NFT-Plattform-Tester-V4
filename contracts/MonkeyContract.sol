090093bbe0e3fa9dd59b949323de2ce45f7f2833
ctrl030 <70302284+ctrl030@users.noreply.github.com>
2021-07-20 18:11:59 -0300

    4 - works, improving

--- contracts/MonkeyContract.sol
+++ contracts/MonkeyContract.sol
@@ -64,9 +64,10 @@ contract MonkeyContract is ERC721Enumerable, Ownable, ReentrancyGuard, Pausable 
         address owner
     );
 
-
+    // xxxx needs zero monkey or out?
     // Constructor function
-    // is setting _name, and _symbol, as well as creating a placeholder NFT,
+    // is setting _name, and _symbol
+    // as well as creating a placeholder NFT,
     // and for that setting _parent1Id, _parent2Id and _generation to 0, 
     // the _genes to the Over 9000 Monkey, and then it is being burnt/locked away  
     constructor() ERC721("Crypto Monkeys", "MONKEY") {
@@ -75,16 +76,154 @@ contract MonkeyContract is ERC721Enumerable, Ownable, ReentrancyGuard, Pausable 
         // minting a placeholder Zero Monkey, that occupies the index 0 position
         //_createMonkey(0, 0, 0, 1214131177989271, _monkeyContractAddress);  
         //createGen0Monkey(1214131177989271);
-
-        // burning Zero Monkey NFT, index 0 can now be treated as "none"      
-        //burnNFT(0); 
     }
 
     // Functions 
     
     function getMonkeyContractAddress() public view returns (address) {  
         return _monkeyContractAddress;
+    }   
+
+    // gives back all the main details on a NFT
+    function getMonkeyDetails(uint256 tokenId)
+        public
+        view
+        returns (
+            uint256 genes,
+            uint256 birthtime,
+            uint256 parent1Id,
+            uint256 parent2Id,
+            uint256 generation,
+            address owner,
+            address approvedAddress
+        )
+    {
+        return (
+            allMonkeysArray[tokenId].genes,
+            allMonkeysArray[tokenId].birthtime,
+            allMonkeysArray[tokenId].parent1Id,
+            allMonkeysArray[tokenId].parent2Id,
+            allMonkeysArray[tokenId].generation,
+            ownerOf(tokenId),
+            getApproved(tokenId)
+        );
     }    
+
+    // XXXXX re-do, not ERC721Enumerable yet
+    // gives back an array with the NFT tokenIds that the provided sender address owns
+    // deleted NFTs are kept as entries with value 0 (token ID 0 is used by Zero Monkey)
+    function findMonkeyIdsOfAddress(address owner) public view returns (uint256[] memory) {
+
+        uint256 amountOwned = balanceOf(owner);
+
+        uint256 entryCounter = 0;
+
+        uint256 totalAmountOfNFTs = totalSupply();
+
+        uint256[] memory ownedTokenIDs = new uint256[](amountOwned);
+
+        for (uint256 tokenIDtoCheck = 0; tokenIDtoCheck < totalAmountOfNFTs; tokenIDtoCheck++ ) {
+            
+            if (ownerOf(tokenIDtoCheck) == owner) {
+                ownedTokenIDs[entryCounter] = tokenIDtoCheck; 
+                entryCounter++;
+            }       
+        }
+
+        return ownedTokenIDs;        
+    }      
+
+    // used for creating gen0 monkeys 
+    function createGen0Monkey(uint256 _genes) public onlyOwner {
+        // making sure that no more than 12 monkeys will exist in gen0
+        require(gen0amountTotal < GEN0_Limit, "Maximum amount of gen 0 monkeys reached");
+
+        // increasing counter of gen0 monkeys 
+        gen0amountTotal++;
+
+        // creating
+        _createMonkey(0, 0, 0, _genes, _msgSender());
+        
+    }    
+    
+    // XXXXX needs to be limited, at the moment anybody can mint anything, can choose genes, owner, generation, unlimited amounts,
+    // how to bind this to frontend inputs? maybe calling randomizing functions in contract first, whose data then get minted, paid at start of randomizing
+    // make this a demo function with generation 99    
+    function createMonkey(
+        uint256 _parent1Id,
+        uint256 _parent2Id,
+        uint256 _generation,
+        uint256 _genes,
+        address _owner
+    ) public returns (uint256) {
+        uint256 newMonkey = _createMonkey(_parent1Id, _parent2Id, _generation, _genes, _owner);
+        return newMonkey;
+    }
+
+    // XXXXX re-do, not ERC721Enumerable yet
+    // used for creating monkeys (returns tokenId, could be used)
+    function _createMonkey(
+        uint256 _parent1Id,
+        uint256 _parent2Id,
+        uint256 _generation,
+        uint256 _genes,
+        address _owner
+    ) private whenNotPaused returns (uint256) {
+        // uses the CryptoMonkey struct as template and creates a newMonkey from it
+            CryptoMonkey memory newMonkey = CryptoMonkey({                
+            parent1Id: uint256(_parent1Id),
+            parent2Id: uint256(_parent2Id),
+            generation: uint256(_generation),
+            genes: _genes,
+            birthtime: uint256(block.timestamp)
+        });        
+        
+        // the push function also returns the length of the array, using that directly and saving it as the ID, starting with 0
+        allMonkeysArray.push(newMonkey);
+        uint256 newMonkeyId = allMonkeysArray.length.sub(1);
+
+        // after creation, transferring to new owner, 
+        // to address is calling user address, sender is 0 address
+        _safeMint(_owner, newMonkeyId);    
+
+        emit MonkeyCreated(_owner, newMonkeyId, _parent1Id, _parent2Id, _genes);
+
+        // xxxx
+        console.log("Token ID %s has genes: %s", newMonkeyId, _genes);
+
+        // tokenId is returned
+        return newMonkeyId;
+    }    
+
+    
+
+    /// * @dev Assign ownership of a specific Monekey to an address.
+    /// * @dev This poses no restriction on _msgSender()
+    /// * @param _from The address from who to transfer from, can be 0 for creation of a monkey
+    /// * @param _to The address to who to transfer to, cannot be 0 address
+    /// * @param _tokenId The id of the transfering monkey
+     
+    function transfer(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public nonReentrant whenNotPaused{
+        require(_to != address(0), "transfer to the zero address");
+        require(_to != address(this), "Can't transfer NFTs to this contract");
+        require (_isApprovedOrOwner(_msgSender(), _tokenId) == true);   
+
+        safeTransferFrom(_from, _to, _tokenId);        
+    }
+
+    function burnNFT (        
+        uint256 _tokenId
+    ) private nonReentrant whenNotPaused{       
+        
+        require (_isApprovedOrOwner(_msgSender(), _tokenId) == true);         
+
+        // burning via openzeppelin
+        _burn(_tokenId);       
+    }
 
     function breed(uint256 _parent1Id, uint256 _parent2Id) public whenNotPaused returns (uint256)  {
 
@@ -119,33 +258,12 @@ contract MonkeyContract is ERC721Enumerable, Ownable, ReentrancyGuard, Pausable 
         return newMonkeyId;
     }
 
-    function _calcGeneration (uint256 _parent1Id, uint256 _parent2Id) internal view returns(uint256) {
-
-        uint256 _generationOfParent1 = allMonkeysArray[_parent1Id].generation; 
-        uint256 _generationOfParent2 = allMonkeysArray[_parent2Id].generation; 
-
-        // new generation is average of parents generations plus 1
-        // for ex. 1 + 5 = 6, 6/2 = 3, 3+1=4, newGeneration would be 4
-
-        // rounding numbers if odd, for ex. 1+2=3, 3*10 = 30, 30/2 = 15
-        // 15 % 10 = 5, 5>0, 15+5=20
-        // 20 / 10 = 2, 2+1 = 3
-        // newGeneration = 3
-        uint256 _roundingNumbers = (((_generationOfParent1 + _generationOfParent2) * 10) / 2); 
-        if (_roundingNumbers % 10 > 0) {
-            _roundingNumbers + 5;      
-        }
-        uint256 newGeneration = (_roundingNumbers / 10 ) + 1;
-
-        return newGeneration;
-    }
-
     /**
     * @dev Returns a binary between 00000000-11111111
     */
     function _getRandom() internal view returns (uint8) {
         return uint8(block.timestamp % 255);
-    }
+    } 
     
     // will generate a pseudo random number and from that decide whether to take mom or dad genes, repeated for 8 pairs of 2 digits each
     function _mixDna (uint256 _parent1genes, uint256 _parent2genes) internal view returns (uint256) {
@@ -214,145 +332,26 @@ contract MonkeyContract is ERC721Enumerable, Ownable, ReentrancyGuard, Pausable 
         return newGeneSequence;      
     }
 
-    // XXXXX re-do, not ERC721Enumerable yet
-    // gives back an array with the NFT tokenIds that the provided sender address owns
-    // deleted NFTs are kept as entries with value 0 (token ID 0 is used by Zero Monkey)
-    function findMonkeyIdsOfAddress(address owner) public view returns (uint256[] memory) {
+    function _calcGeneration (uint256 _parent1Id, uint256 _parent2Id) internal view returns(uint256) {
 
-        uint256 amountOwned = balanceOf(owner);
+        uint256 _generationOfParent1 = allMonkeysArray[_parent1Id].generation; 
+        uint256 _generationOfParent2 = allMonkeysArray[_parent2Id].generation; 
 
-        uint256 entryCounter = 0;
+        // new generation is average of parents generations plus 1
+        // for ex. 1 + 5 = 6, 6/2 = 3, 3+1=4, newGeneration would be 4
 
-        uint256 totalAmountOfNFTs = totalSupply();
-
-        uint256[] memory ownedTokenIDs = new uint256[](amountOwned);
-
-        for (uint256 tokenIDtoCheck = 0; tokenIDtoCheck < totalAmountOfNFTs; tokenIDtoCheck++ ) {
-            
-            if (ownerOf(tokenIDtoCheck) == owner) {
-                ownedTokenIDs[entryCounter] = tokenIDtoCheck; 
-                entryCounter++;
-            }       
+        // rounding numbers if odd, for ex. 1+2=3, 3*10 = 30, 30/2 = 15
+        // 15 % 10 = 5, 5>0, 15+5=20
+        // 20 / 10 = 2, 2+1 = 3
+        // newGeneration = 3
+        uint256 _roundingNumbers = (((_generationOfParent1 + _generationOfParent2) * 10) / 2); 
+        if (_roundingNumbers % 10 > 0) {
+            _roundingNumbers + 5;      
         }
+        uint256 newGeneration = (_roundingNumbers / 10 ) + 1;
 
-        return ownedTokenIDs;        
+        return newGeneration;
     }
-
-    // used for creating gen0 monkeys 
-    function createGen0Monkey(uint256 _genes) public onlyOwner {
-        // making sure that no more than 12 monkeys will exist in gen0
-        require(gen0amountTotal < GEN0_Limit, "Maximum amount of gen 0 monkeys reached");
-
-        // increasing counter of gen0 monkeys 
-        gen0amountTotal++;
-
-        // creating
-        _createMonkey(0, 0, 0, _genes, _msgSender());
-        
-    }
-
-    // XXXXX re-do, not ERC721Enumerable yet
-    // used for creating monkeys (returns tokenId, could be used)
-    function _createMonkey(
-        uint256 _parent1Id,
-        uint256 _parent2Id,
-        uint256 _generation,
-        uint256 _genes,
-        address _owner
-    ) private whenNotPaused returns (uint256) {
-        // uses the CryptoMonkey struct as template and creates a newMonkey from it
-            CryptoMonkey memory newMonkey = CryptoMonkey({                
-            parent1Id: uint256(_parent1Id),
-            parent2Id: uint256(_parent2Id),
-            generation: uint256(_generation),
-            genes: _genes,
-            birthtime: uint256(block.timestamp)
-        });
-        // updating total supply
-        //totalSupply++;
-        
-        // the push function also returns the length of the array, using that directly and saving it as the ID, starting with 0
-        allMonkeysArray.push(newMonkey);
-        uint256 newMonkeyId = allMonkeysArray.length.sub(1);
-
-        // after creation, transferring to new owner, 
-        // transferring address is user, sender is 0 address
-        _safeMint(_owner, newMonkeyId);    
-
-        emit MonkeyCreated(_owner, newMonkeyId, _parent1Id, _parent2Id, _genes);
-
-        // xxxx
-        console.log("Token ID %s has genes: %s", newMonkeyId, _genes);
-
-        // tokenId is returned
-        return newMonkeyId;
-    }    
-    
-    function createMonkey(
-        uint256 _parent1Id,
-        uint256 _parent2Id,
-        uint256 _generation,
-        uint256 _genes,
-        address _owner
-    ) public returns (uint256){
-        uint256 newMonkey = _createMonkey(_parent1Id, _parent2Id, _generation, _genes, _owner);
-        return newMonkey;
-    }
-
-    // gives back all the main details on a NFT
-    function getMonkeyDetails(uint256 tokenId)
-        public
-        view
-        returns (
-            uint256 genes,
-            uint256 birthtime,
-            uint256 parent1Id,
-            uint256 parent2Id,
-            uint256 generation,
-            address owner,
-            address approvedAddress
-        )
-    {
-        return (
-            allMonkeysArray[tokenId].genes,
-            allMonkeysArray[tokenId].birthtime,
-            allMonkeysArray[tokenId].parent1Id,
-            allMonkeysArray[tokenId].parent2Id,
-            allMonkeysArray[tokenId].generation,
-            ownerOf(tokenId),
-            getApproved(tokenId)
-        );
-    }   
-
-    /// * @dev Assign ownership of a specific Monekey to an address.
-    /// * @dev This poses no restriction on _msgSender()
-    /// * @param _from The address from who to transfer from, can be 0 for creation of a monkey
-    /// * @param _to The address to who to transfer to, cannot be 0 address
-    /// * @param _tokenId The id of the transfering monkey
-     
-    function transfer(
-        address _from,
-        address _to,
-        uint256 _tokenId
-    ) public nonReentrant whenNotPaused{
-        require(_to != address(0), "transfer to the zero address");
-        require(_to != address(this), "Can't transfer to");
-        require (_isApprovedOrOwner(_msgSender(), _tokenId) == true);   
-
-        safeTransferFrom(_from, _to, _tokenId);
-        
-    }
-
-    function burnNFT (        
-        uint256 _tokenId
-    ) private nonReentrant whenNotPaused{       
-        
-        require (_isApprovedOrOwner(_msgSender(), _tokenId) == true);         
-
-        // burning via openzeppelin
-        _burn(_tokenId);       
-    }
-
     
 }
 
